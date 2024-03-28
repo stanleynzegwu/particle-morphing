759f3d0c7b97a46eb17594806c3cf010d0c367b8
Stanley Nzegwu <nzegwustanley@gmail.com>
2024-03-28 10:06:28 +0100

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { Suspense } from "react";
 import Experience from "./canvas/Experience";
-import Section1 from "./components/section1";
-import Section2 from "./components/Section2";
+// import Section1 from "./components/section1";
+// import Section2 from "./components/Section2";
 
 function App() {
   return (
     <div className="main w-full bg-black">
-      <div className="h-screen w-full fixed top-0 z-10 pointer-events-none">
+      <div className="h-screen w-full fixed top-0 bg-black z-10 pointer-events-none">
         <Suspense fallback={null}>
           <Experience />
         </Suspense>
       </div>
-      <Section1 />
-      <Section2 />
+      {/* <Section1 />
+      <Section2 /> */}
     </div>
   );
 }
